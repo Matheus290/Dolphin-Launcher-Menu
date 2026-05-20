c66f48b1e4a9836ca159f5471c03e8114d66c755
Matheus290 <matheus-290@hotmail.com>
2026-05-20 15:52:24 -0300

    Update uninstall-user.sh

--- install/uninstall-user.sh
+++ install/uninstall-user.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 rm -f "$HOME/.local/bin/create-launcher"
 rm -f "$HOME/.local/share/kio/servicemenus/create-launcher.desktop"
 
-# Atualiza cache do KDE (não é obrigatório, mas ajuda)
 kbuildsycoca6 2>/dev/null || kbuildsycoca5 || true
 
 echo "Uninstalled."
